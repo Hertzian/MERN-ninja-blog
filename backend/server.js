1cd148c84c32c3409645344b3fb2782a61298913
Lalitus <tallitosan@gmail.com>
2021-01-07 21:02:48 -0600

    added NODE_ENV into server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const dotenv = require('dotenv')
 const express = require('express')
 dotenv.config()
@@ -11,6 +12,12 @@ const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 app.use(express.json())
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
 
 // route list
 app.use('/api/users', userRoutes)
