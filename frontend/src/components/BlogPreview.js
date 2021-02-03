ea0dc9a5b5859bc36a335af0778b3a7d1a4bf6f2
Lalo Aguilar <tallitosan@gmail.com>
2021-02-03 13:46:34 -0600

    update stylesheet, conditional button rendering in BlogPreview

--- frontend/src/components/BlogPreview.js
+++ frontend/src/components/BlogPreview.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import AuthContext from '../context/auth/authContext'
 
-const BlogPreview = ({title, id, author}) => {
+const BlogPreview = ({ title, id, author }) => {
+  const authContext = useContext(AuthContext)
+  const { user } = authContext
+  console.log(user)
+
   return (
-    <div className="blog-preview">
+    <div className='blog-preview'>
       <h2>{title}</h2>
       <p>by {author.name}</p>
-      <Link to={`/post/${id}`}>view more</Link>
+      <Link to={`/post/${id}`}>View more</Link>
+      {user.name === author.name && (
+        <>
+          <Link to={`/update/${id}`} >Update</Link>
+          <button>Delete</button>
+        </>
+      )}
     </div>
   )
 }
